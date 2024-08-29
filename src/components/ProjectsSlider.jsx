9fdfa116d86e7adb3723be3f7631df02cfefa87f
masud1901 <akmolmasud5@gmail.com>
2024-08-29 13:13:14 +0600

    The project swiper is almost complete

--- src/components/ProjectsSlider.jsx
+++ src/components/ProjectsSlider.jsx
@@ -1,63 +1,55 @@
-import { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import "../css/ProjectsSlider.css";
+import "../css/ProjectsSlider.css"; // Updated CSS file name for uniqueness
 
 const TrendingSlider = () => {
   const handleButtonClick = () => {
-    // Add your button click logic here
     console.log("Button clicked!");
   };
+
   const slides = [
     {
-      img: "src/assets/heroSection1.jpeg",
-      price: "$20",
-      name: "Special Pizza",
-      rating: 4.5,
+      img: "src/assets/project_images/1.png",
+      header: "Project 1",
+      description: "This is a brief description of Project 1.",
     },
     {
-      img: "src/assets/heroSection2.jpeg",
-      price: "$20",
-      name: "A Guy Working",
-      rating: 4.5,
+      img: "src/assets/project_images/2.png",
+      header: "Project 2",
+      description: "This is a brief description of Project 2.",
     },
     {
-      img: "src/assets/heroSection3.jpeg",
-      price: "$40",
-      name: "A Guy Welding",
-      rating: 4.5,
+      img: "src/assets/project_images/3.png",
+      header: "Project 3",
+      description: "This is a brief description of Project 3.",
     },
     {
-      img: "src/assets/heroSection1.jpeg",
-      price: "$20",
-      name: "A Guy",
-      rating: 4.5,
+      img: "src/assets/project_images/4.png",
+      header: "Project 4",
+      description: "This is a brief description of Project 4.",
     },
     {
-      img: "src/assets/heroSection2.jpeg",
-      price: "$20",
-      name: "Meat Ball",
-      rating: 4.5,
+      img: "src/assets/project_images/5.png",
+      header: "Project 5",
+      description: "This is a brief description of Project 5.",
     },
     {
-      img: "src/assets/heroSection3.jpeg",
-      price: "$40",
-      name: "Burger",
-      rating: 4.5,
+      img: "src/assets/project_images/6.png",
+      header: "Project 6",
+      description: "This is a brief description of Project 6.",
     },
   ];
 
   return (
-    <section id="Projects">
-      <div className="projects-container">
-        {/* <h3 className="text-center section-subheading">- popular Delivery -</h3> */}
-        <h1 className="text-center section-heading">Projects</h1>
+    <section id="CustomProjects">
+      <div className="custom-projects-container">
+        <h1 className="custom-section-heading">Projects</h1>
       </div>
-      <div className="swiper-container">
+      <div className="custom-swiper-container">
         <Swiper
           effect={"coverflow"}
           grabCursor={true}
@@ -70,48 +62,38 @@ const TrendingSlider = () => {
             depth: 100,
             modifier: 2.5,
           }}
-          pagination={{ el: ".swiper-pagination", clickable: true }}
+          pagination={{ el: ".custom-swiper-pagination", clickable: true }}
           navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            nextEl: ".custom-swiper-button-next",
+            prevEl: ".custom-swiper-button-prev",
           }}
           modules={[EffectCoverflow, Pagination, Navigation]}
-          className="projects-slider"
+          className="custom-projects-slider"
         >
           {slides.map((slide, index) => (
-            <SwiperSlide key={index} className="projects-slide">
-              <div className="projects-slide-img">
-                <img src={slide.img} alt="projects" />
+            <SwiperSlide key={index} className="custom-projects-slide">
+              <div className="custom-projects-slide-img">
+                <img src={slide.img} alt={slide.header} />
               </div>
-              <div className="projects-slide-content">
-                {/* <h1 className="food-price">{slide.price}</h1> */}
-                <div className="projects-slide-content-bottom">
-                  <h2 className="food-name">{slide.name}</h2>
-                  <h3 className="food-rating">
-                    <span>{slide.rating}</span>
-                    <div className="rating">
-                      {[...Array(5)].map((_, i) => (
-                        <ion-icon key={i} name="star"></ion-icon>
-                      ))}
-                    </div>
-                  </h3>
-                </div>
+              <div className="custom-projects-slide-content">
+                <h2>{slide.header}</h2>
+                <p>{slide.description}</p>
               </div>
             </SwiperSlide>
           ))}
-          <div className="projects-slider-control">
-            <div className="swiper-button-prev slider-arrow">
+          <div className="custom-projects-slider-control">
+            <div className="custom-swiper-button-prev custom-slider-arrow">
               <ion-icon name="arrow-back-outline"></ion-icon>
             </div>
-            <div className="swiper-button-next slider-arrow">
+            <div className="custom-swiper-pagination"></div>
+            <div className="custom-swiper-button-next custom-slider-arrow">
               <ion-icon name="arrow-forward-outline"></ion-icon>
             </div>
-            <div className="swiper-pagination"></div>
           </div>
         </Swiper>
       </div>
-      <div className="button-projects-container">
-        <button className="view-all-button" onClick={handleButtonClick}>
+      <div className="custom-button-projects-container">
+        <button className="custom-view-all-button" onClick={handleButtonClick}>
           View All Projects
         </button>
       </div>
