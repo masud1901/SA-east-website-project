eda4f8c089e6e09e02e1c95cae6c183c5dcf8697
masud1901 <akmolmasud5@gmail.com>
2024-09-01 16:10:57 +0600

    Contact us part done

--- src/components/GoalsSection.jsx
+++ src/components/GoalsSection.jsx
@@ -9,35 +9,36 @@ const Goals = () => {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-12 col-md-4">
-            <div className="goal-card">
+            <div className="goal-card goal-card-1">
               <h3 className="goal-card-title">Provide The Best Products</h3>
               <p className="goal-card-text">
-                Our goals are to provide the best products, services and
+                Our goals are to provide the best products, services, and
                 technology in the Kingdom of Saudi Arabia and the other markets
-                in which we operate.
+                in which our operation is done.
               </p>
             </div>
           </div>
           <div className="col-12 col-md-4">
-            <div className="goal-card">
+            <div className="goal-card goal-card-2">
               <h3 className="goal-card-title">
-                Maintaining distinguished partnerships
+                Maintaining Distinguished Partnerships
               </h3>
               <p className="goal-card-text">
                 Maintaining distinguished partnerships with our customers,
-                employees and partners in the Kingdom of Saudi Arabia and other
+                employees, and partners in the Kingdom of Saudi Arabia and other
                 markets.
               </p>
             </div>
           </div>
           <div className="col-12 col-md-4">
-            <div className="goal-card">
+            <div className="goal-card goal-card-3">
               <h3 className="goal-card-title">
-                Provide customers with the best products
+                Provide Customers with the Best Products
               </h3>
               <p className="goal-card-text">
                 Our vision includes providing customers with the premium
-                products and specialized services they need in today{"'"}s world
+                products and specialized services they need in today{"'"}s
+                world.
               </p>
             </div>
           </div>
