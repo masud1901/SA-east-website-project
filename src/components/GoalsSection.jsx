f0106c1411d8d2de03a7ca8611d52ea4e8395084
overaisul <overaisulislam@gmail.com>
2025-03-09 12:20:39 +0600

    test

--- src/components/GoalsSection.jsx
+++ src/components/GoalsSection.jsx
@@ -29,6 +29,7 @@ const Goals = () => {
                 markets.
               </p>
             </div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugit aut harum labore facere, eveniet pariatur, earum aliquam debitis error non unde sapiente tempora minus impedit repellat itaque saepe? Perspiciatis quos sit temporibus veniam libero reiciendis quia commodi vero sed quaerat rerum fuga nisi, alias expedita optio atque quae cupiditate minima dignissimos consequatur labore officia. Ea doloribus ipsum tempore vero doloremque officiis, reprehenderit itaque delectus autem voluptate ullam consequuntur culpa labore ipsa id corrupti laudantium fuga rem quasi, cupiditate sequi quae voluptates. Ipsam placeat dicta, tenetur temporibus, explicabo qui ullam, eaque fuga rerum illo iste repellat itaque? Sed, ut aliquam!</div>
           </div>
           <div className="col-12 col-md-4">
             <div className="goal-card goal-card-3">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -92,6 +92,7 @@ const Header = () => {
               <Link to="/project" onClick={scrollToTop}>
                 PROJECT
               </Link>
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugit aut harum labore facere, eveniet pariatur, earum aliquam debitis error non unde sapiente tempora minus impedit repellat itaque saepe? Perspiciatis quos sit temporibus veniam libero reiciendis quia commodi vero sed quaerat rerum fuga nisi, alias expedita optio atque quae cupiditate minima dignissimos consequatur labore officia. Ea doloribus ipsum tempore vero doloremque officiis, reprehenderit itaque delectus autem voluptate ullam consequuntur culpa labore ipsa id corrupti laudantium fuga rem quasi, cupiditate sequi quae voluptates. Ipsam placeat dicta, tenetur temporibus, explicabo qui ullam, eaque fuga rerum illo iste repellat itaque? Sed, ut aliquam!</div>
               <Link to="/about" onClick={scrollToTop}>
                 ABOUT
               </Link>
